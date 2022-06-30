a1831459d19c8ac17a16b830bab76861930ef2c9
KavyaSriSunkoji <97873072+KavyaSriSunkoji@users.noreply.github.com>
2022-06-30 22:38:08 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,25 @@
+var x=Math.floor(Math.random()*6)+1;
+var y=Math.floor(Math.random()*6)+1;
+if(x===1) {document.querySelector(".img1").setAttribute("src","dice1.png");}
+else if(x===2){ document.querySelector(".img1").setAttribute("src","dice2.png");}
+else if(x===3){document.querySelector(".img1").setAttribute("src","dice3.png");}
+else if(x===4){ document.querySelector(".img1").setAttribute("src","dice4.png");}
+else if(x===5){ document.querySelector(".img1").setAttribute("src","dice5.png");}
+else{ document.querySelector(".img1").setAttribute("src","dice6.png");}
+
+if(y===1) {document.querySelector(".img2").setAttribute("src","dice1.png");}
+else if(y===2){ document.querySelector(".img2").setAttribute("src","dice2.png");}
+else if(y===3){ document.querySelector(".img2").setAttribute("src","dice3.png");}
+else if(y===4){ document.querySelector(".img2").setAttribute("src","dice4.png");}
+else if(y===5){ document.querySelector(".img2").setAttribute("src","dice5.png");}
+else{ document.querySelector(".img2").setAttribute("src","dice6.png");}
+
+document.querySelector(".img-text1").innerHTML="player1:"+x;
+document.querySelector(".img-text2").innerHTML='player2:"+y;
+if(x>y){
+  document.querySelector(".result").innerHTML="player 1 WON!!!";
+}
+else if (x<y) {
+  document.querySelector(".result").innerHTML="player 2 WON!!!";
+}
+else document.querySelector(".result").innerHTML="ITS A TIE!!!";
